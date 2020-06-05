86b410b8eb143d258b6c5c9a66461367b51ff3ea
Akshay Sachdeva <asachdeva@citrine.io>
2020-06-05 17:37:06 -0500

    latest

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo -t winning # if using a theme, replace with `hugo -t <YOURTHEME>`
+#hugo -t winning # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo
 
 # Go To Public folder
 cd public
